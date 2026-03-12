9b2d6effd8d5239dce6f3b56d8445bd65ffdc5ec
Thorsteinnk92 <thorsteinnk92@gmail.com>
2026-03-12 10:44:04 +0000

    Henti steina index

--- index.js
+++ index.js
@@ -1,43 +1,21 @@
 /* --------------------------
 
-   INITIAL EXPRESS CONFIG  
-   (Middleware, CORS, JSON)
+    INITIAL EXPRESS CONFIG  
+    (Middleware, CORS, JSON)
 
 -------------------------- */
 
 import express, { json } from "express";
+
+/* Use cors to avoid issues with testing on localhost */
 import cors from "cors";
-import { events, attendees } from "./data/initialData.js";
-import { getNextEventId, getNextAttendeeId } from "./data/initialData.js";
-
-
 
 const app = express();
-
 
 /* Base url parameters and port settings */
 const apiPath = "/api/";
 const version = "v1";
 const port = 3000;
-const parseId = (value) => {
-  const n = Number(value)
-  if(Number.isInteger(n) && n > 0) {
-    return n;
-  } else {
-    return null;
-  }
-
-}
-const validateEventId = (req, res, next) => {
-  const eventId = parseId(req.params.eventId);
-  if (!eventId) {
-    return res.status(400).json({ message: "EventID not valid" })
-  }
-  req.eventId = eventId
-  next();
-}
-
-
 
 /* Set Cors-related headers to prevent blocking of local requests */
 app.use(json());
@@ -53,95 +31,90 @@ app.use((req, res, next) => {
 });
 
 /* Initial Data */
+import { events, attendees } from "./data/initialData.js";
+import { getNextEventId, getNextAttendeeId } from "./data/initialData.js";
 
-// app.get(`${apiPath}${version}/events`, (req, res) => {
-  
-const api = express.Router();
-app.use("/api/v1", api)
-  
+/* --------------------------
+
+      HELPER FUNCTIONS    
+
+-------------------------- */
+
+const parseId = (value) => {
+  const n = Number(value);
+  if (Number.isInteger(n) && n > 0) {
+    return n;
+  }
+  else {
+    return null;
+  }
+};
+
+const validateEventId = (req, res, next) => {
+  const eventId = parseId(req.params.eventId);
+  if (!eventId) {
+    return res.status(400).json({message: "Event ID invalid, must be positive integer"
+      
+    });
+  }
+  req.eventId = eventId;
+  next();
+}
+
+const ensureEventExists = (req, res, next) => {
+  const event = events.find(e => e.id === req.eventId);
+  if (!event) {
+    return res.status(404).json({
+      message: "Event not found"
+    })
+  }
+  req.event = event;
+  next();
+};
+
+
 /* --------------------------
 
       EVENTS ENDPOINTS     
 
 -------------------------- */
 
-/* --------------------------
-//Endpoint: read all events
-Method: GET
-Url: /api/v1/events
 
-Endpoint: Read a specifi event
-If endpoint is invalid request returns 400
-if successful returns 200
+app.get('/api/v1/events',  (req, res) => {
+  const allowedParams = ['name', 'location'];
+  const receivedParams = Object.keys(req.query);
 
-*/
-api.post("/api/v1/events", (req,res) => {
-  const {name, location, date} = req.body
-}
-)
-api.get("/events", (req, res) => {
-  const {name, location, ...rest} = req.query
-
-  if (Object.keys(rest).length > 0) {
-    return res.status(400).json({message: "Invalid query param"})
+  const hasInvalidParams = receivedParams.some(p => !allowedParams.includes(p));
+  if (hasInvalidParams) {
+    return res.status(400).json({ message: "Invalid query parameter" })
   }
 
-  let filteredData = events
+  const { name, location } = req.query;
+
+  let results = events;
 
   if (name) {
-    filteredData = filteredData.filter(e =>
-      e.name.toLowerCase().includes(name.toLowerCase())
-    )
+    results = results.filter(e => e.name.toLocaleLowerCase().includes(name.toLocaleLowerCase())
+    );
   }
   if (location) {
-    filteredData = filteredData.filter(e => 
-      e.location.toLowerCase().includes(location.toLowerCase())
-    )
+    results = results.filter(e => e.location.toLocaleLowerCase().includes(location.toLowerCase())
+    );
   }
-  res.status(200).json(filteredData)
-})
 
-api.get("/events/:eventId", validateEventId, (req, res) => {
-  const event = events.find(e => e.id === req.eventId)  
-  if (!event) {
-    return res.status(404).json({ message: "Event not found" })
-  }
-  const attendeeCount = attendees.filter(a => a.eventIds.includes(req.eventId)).length
-  res.status(200).json({ ...event, attendeeCount })
-})
-api.get("/attendees", (req, res) => {
-  res.status(200).json(attendees)
-})
-/*
+  return res.status(200).json(results);
+});
 
-
+/* --------------------------
 
     ATTENDEES ENDPOINTS    
 
 -------------------------- */
+app.get('/api/v1/attendees',  (req, res) => {
+  return res.status(200).json(attendees);
+});
 
-//Endpoint: Read all attendees
-//If non exist, an empty array is returned
-//The request returns status 200
-//Method: Get
-// Url: /api/v1/events/attendees
-/*
-Endpoint: create a new event
-*/
-api.use((req, res) => {
-  res.status(404).json({ message: "Endpoint not found" })
-})
-/* --------------------------
-
-      SERVER INITIALIZATION  
-      
-!! DO NOT REMOVE OR CHANGE THE FOLLOWING (IT HAS TO BE AT THE END OF THE FILE) !!
-      
--------------------------- */
-if (process.env.NODE_ENV !== "test") {
-  app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
-  });
-}
-
-export default app;
+app.get('/api/v1/events/:eventId', validateEventId, ensureEventExists, (req, res) => {
+  const attendeeCount = attendees.filter(a => a.eventIds.includes(req.eventId)).length;
+  return res.status(200).json({ ...req.event, attendeeCount})
+});
